29bcf9f65fed0a284ab26c75a905fe097e6c7067
fool <jester@fool.ltd>
2025-03-01 01:15:40 -0600

    Push 2

---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+# Activate your virtual environment if not already activated
+# On Windows:
+# .venv\Scripts\activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
